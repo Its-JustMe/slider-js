f170ca44e313098eff5c6fed1ff0058917e90e76
Arthur <arthursantos48211@gmail.com>
2023-04-05 11:57:00 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
-import { mudarClasseBtn } from "./modules.js";
-import { trocarSlide } from './modules.js';
+//import { mudarClasseBtn } from "./modules.js";
+import { mudarClasseBtn, trocarSlide, trocarTexto } from './modules.js';
 
 const btn_slider = document.querySelectorAll('.select');
-const btn_slider_checked = document.querySelector('.select.selected');
-
 const slides = document.querySelectorAll('video');
+const textos = document.querySelectorAll('.slider');
 
 let slideAtual = slides[0];
 let slideProximo = slides[1];
+
+let textoAtual = textos[0];
+let proximoTexto = textos[1];
+
+let primeiroBtn = btn_slider[0];
+let proximoBtn = btn_slider[1];
 
 setInterval(() => {
     trocarSlide(slideAtual, slideProximo);
     slideAtual = slideProximo;
     slideProximo = slideAtual.nextElementSibling || slides[0];
-}, 8000);
+
+    trocarTexto(textoAtual, proximoTexto);
+    textoAtual = proximoTexto;
+    proximoTexto = textoAtual.nextElementSibling || textos[0];
+
+    mudarClasseBtn(primeiroBtn, proximoBtn);
+    primeiroBtn = proximoBtn;
+    proximoBtn = primeiroBtn.nextElementSibling || btn_slider[0];
+}, 9000);
