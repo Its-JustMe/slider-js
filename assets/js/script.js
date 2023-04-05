6e4c0eff9168ccb1aa85edfa8c8b2aafad6acee1
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-05 00:52:22 -0300

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//import { mudarClasseBtn } from "./modules.js";
+import { trocarSlide } from './modules.js';
+
+const btn_slider = document.querySelectorAll('.select');
+const btn_slider_checked = document.querySelector('.select.selected');
+
+const slides = document.querySelectorAll('video');
+
+let slideAtual = slides[0];
+let slideProximo = slides[1];
+
+setInterval(() => {
+    trocarSlide(slideAtual, slideProximo);
+    slideAtual = slideProximo;
+    slideProximo = slideAtual.nextElementSibling || slides[0];
+}, 9000);
