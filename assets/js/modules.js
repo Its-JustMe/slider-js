7618ee53b0f100fcf872fcc1b86a595f1a995b44
Arthur <arthursantos48211@gmail.com>
2023-04-05 12:03:19 -0300

    .

--- assets/js/modules.js
+++ assets/js/modules.js
@@ -12,22 +12,3 @@ export function trocarSlide(slideAtual, proximoSlide) {
     slideAtual.classList.remove('active');
     proximoSlide.classList.add('active');
 }
-
-export function mudarClasseBtn (button, all_btn) {
-    if (button.classList.contains('selected')) {
-        button.classList.remove('selected');
-    } else {
-        button.classList.add('selected');
-    }
-
-    all_btn.forEach(btn => {
-        if (btn !== button && btn.classList.contains('selected')) {
-            btn.classList.remove('selected');
-        }
-    });
-}
-
-export function trocarSlide(slideAtual, proximoSlide, btn) {
-    slideAtual.classList.remove('active');
-    proximoSlide.classList.add('active');
-}
