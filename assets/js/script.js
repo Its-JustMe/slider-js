332500c047d0beb49b2baae08a3de9646dcc7e67
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-06 18:51:14 -0300

    Configurando o slideshow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
-import { slideShow } from './modules.js';
+/** Função que controla o slideshow manualmente */
+function slideShow(buttons, sliders, videos) {
+    for (let btn in buttons) {
+      buttons[btn].classList.remove('selected');
+    }
+    this.classList.add('selected');
+
+    for (let video in videos) {
+      videos[video].classList.remove('active');
+    }
+
+    document.querySelector(`video[name="${this.name}"]`).classList.add('active');
+
+    for (let slide in sliders) {
+      sliders[slide].classList.remove('active');
+    }
+
+    sliders[i].classList.add('active');
+}
 
 const buttons = document.querySelectorAll('.select');
 const sliders = document.querySelectorAll('.slider');
 const videos = document.querySelectorAll('.bg-video');
 
-for (let i = 0; i < buttons.length; i++) {
+for (let i in buttons) {
   buttons[i].addEventListener('click', slideShow());
 }
