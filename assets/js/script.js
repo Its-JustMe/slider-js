c28406f018f95251a2f150fe6dcfa74f2fcb9688
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-06 18:48:17 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-import { slideShow } from "./modules.js";
+import { slideShow } from './modules.js';
 
 const buttons = document.querySelectorAll('.select');
 const sliders = document.querySelectorAll('.slider');
 const videos = document.querySelectorAll('.bg-video');
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', slideShow(buttons, sliders, videos));
+  buttons[i].addEventListener('click', slideShow());
 }
