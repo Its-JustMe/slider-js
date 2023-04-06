211ea734ecf3d0a3093014c2f5c9ae4d355d2070
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-06 18:38:41 -0300

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,28 @@
-import { mudarClasseBtn, trocarSlide, trocarTexto } from './modules.js';
+const buttons = document.querySelectorAll('.select');
+const sliders = document.querySelectorAll('.slider');
+const videos = document.querySelectorAll('.bg-video');
 
-const btn_slider = document.querySelectorAll('.select');
-const slides = document.querySelectorAll('video');
-const textoSlides = document.querySelectorAll(".slider");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    // Remove a classe "selected" de todos os botões
+    for (let j = 0; j < buttons.length; j++) {
+      buttons[j].classList.remove('selected');
+    }
+    // Adiciona a classe "selected" ao botão clicado
+    this.classList.add('selected');
 
-let slideAtual = slides[0];
-let slideProximo = slides[1];
+    // Remove a classe "active" de todos os vídeos
+    for (let j = 0; j < videos.length; j++) {
+      videos[j].classList.remove('active');
+    }
+    // Adiciona a classe "active" ao vídeo com o nome correspondente ao botão clicado
+    document.querySelector(`video[name="${this.name}"]`).classList.add('active');
 
-let primeiroBtn = btn_slider[0];
-let proximoBtn = btn_slider[1];
-
-let textoAtual = textoSlides[0];
-let textoProximo = textoSlides[1];
-
-setInterval(() => {
-    trocarSlide(slideAtual, slideProximo);
-    slideAtual = slideProximo;
-    slideProximo = slideAtual.nextElementSibling || slides[0];
-
-    mudarClasseBtn(primeiroBtn, proximoBtn);
-    primeiroBtn = proximoBtn;
-    proximoBtn = primeiroBtn.nextElementSibling || btn_slider[0];
-
-    trocarTexto(textoAtual, textoProximo, textoSlides);
-    textoAtual = textoProximo;
-    textoProximo = textoAtual.nextElementSibling || textoSlides[0];
-
-}, 9000);
+    // Remove a classe "active" de todos os sliders
+    for (let j = 0; j < sliders.length; j++) {
+      sliders[j].classList.remove('active');
+    }
+    // Adiciona a classe "active" ao slider com o índice correspondente ao botão clicado
+    sliders[i].classList.add('active');
+  });
+}
