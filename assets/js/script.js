f3e938be4533e452e9bc9a1b4cb701384e591574
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-06 20:24:12 -0300

    Adicionando animação ao ícone de menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ for (let i = 0; i < buttons.length; i++) {
 menuBtn.addEventListener('click', function () {
     alert('Clicou');
     if (menuBtn.classList.contains('animate')) {
-        this.classList.add('animate');
+        this.classList.toggle('animate');
     } else {
         this.classList.remove('animate');
     }
