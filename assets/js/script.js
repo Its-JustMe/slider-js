2f51ac4611221b0d164ec480b0d1a9f1aafb8aa6
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-04-06 19:41:41 -0300

    Adicionando efeito de transição ao slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const videos = document.querySelectorAll('.bg-video');
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function() {
+    document.querySelector('div.alternate').classList.add('active');
     for (let j = 0; j < buttons.length; j++) {
       buttons[j].classList.remove('selected');
     }
@@ -15,13 +16,11 @@ for (let i = 0; i < buttons.length; i++) {
     }
 
     document.querySelector(`video[name="${this.name}"]`).classList.add('active');
-    document.querySelector('.alternate').classList.add('active');
 
     for (let j = 0; j < sliders.length; j++) {
       sliders[j].classList.remove('active');
     }
 
     sliders[i].classList.add('active');
-    document.querySelector('.alternate').classList.remove('active');
   });
 }
